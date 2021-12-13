d5d2cffce8e4be896d912fab629a4b3d57110db6
19PA1A0478 <60468171+19PA1A0478@users.noreply.github.com>
2021-12-13 10:27:23 +0530

    Create Bounce.java

---
+++ Bounce.java
@@ -0,0 +1,19 @@
+//if any position contains snake or ladder the player needs to Bounce.
+
+public class Bounce {
+    private int start;
+    private int end;
+    public Bounce(int start,int end){
+        this.start = start;
+        this.end = end;
+    }
+    public int getStart(){
+        return start;
+    }
+
+    public int getEnd(){
+        return end;
+    }
+}
+
+
